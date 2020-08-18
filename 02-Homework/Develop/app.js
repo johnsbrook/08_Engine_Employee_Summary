5bab80495a8fe86c72b5d5f2610830cae1440e29
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-17 22:52:39 -0400

    added email validation

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -29,106 +29,115 @@ inquirer.prompt([
         name: "id"
     },
     {
-        type: "input",
-        message: "What's the employee\'s email?",
-        name: "email"
-    },
-    {
         type: "list",
         message: "What's the employee\'s title?",
         name: "role",
         choices: ["Manager", "Engineer", "Intern"]
     }
 ]).then(function(r){
+
     this.name = r.name;
-      this.id = r.id;
-        this.email = r.email;
-            this.role = r.role;
+    this.id = r.id;
+    this.role = r.role;
    
-}).then(function(){
-
-    if (validator.validate(this.email)) {
-
-        if (this.role === "Manager") {
+    }).then(function(){
+        email();
+        function email() {
             inquirer.prompt([
                 {
                     type: "input",
-                    message: "What's the manager\'s office number?",
-                    name: "officeNumber", 
+                    message: "What's the employee\'s email?",
+                    name: "email"
                 }
-            ]).then(function(r2) {
-                this.officeNumber = r2.officeNumber;
-    
-                console.log(this.name);
-                console.log(this.id);
-                console.log(this.email);
-                console.log(this.role);
-                console.log(this.officeNumber);
-    
-                const manager = new Manager(this.name, this.id, this.email, this.officeNumber);
-                console.log(manager);
-                
-                team.push(manager);
-                
-            }).then(function() {
-                addMore(); 
-            })
-    
-        } else if (this.role === "Engineer") {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "What's the engineer\'s github username?",
-                    name: "github", 
-                }
-    
-            ]).then(function(r3) {
-                this.github = r3.github;
-    
-                console.log(this.name);
-                console.log(this.id);
-                console.log(this.email);
-                console.log(this.role);
-                console.log(this.github);
-    
-                const engineer = new Engineer(this.name, this.id, this.email, this.github);
-                console.log(engineer)
-                team.push(engineer);
-                
-                
-                }).then(function() {
-                    addMore(); 
-                })
-    
-            } else {
-                inquirer.prompt([
-                    {
-                    type: 'input',
-                    message: "What's the Intern\'s school name?",
-                    name: 'school'
-                    }
-                ]).then(function(r2){
-                    this.school = r2.school;
-                    console.log(this.school);
-    
-                    const school = new Intern(this.name, this.id, this.email, this.school);                
-                    console.log(school);
-                    team.push(school);
+                ]).then(function(re){
+
+                    this.email = re.email;
+
+                    if (validator.validate(this.email)) {
+                        this.email = re.email;
+
+                        if (this.role === "Manager") {
+                            inquirer.prompt([
+                                {
+                                    type: "input",
+                                    message: "What's the manager\'s office number?",
+                                    name: "officeNumber", 
+                                }
+                            ]).then(function(r2) {
+                                this.officeNumber = r2.officeNumber;
                     
-    
-                }).then(function() {
-                    addMore(); 
-                })
+                                console.log(this.name);
+                                console.log(this.id);
+                                console.log(this.email);
+                                console.log(this.role);
+                                console.log(this.officeNumber);
+                    
+                                const manager = new Manager(this.name, this.id, this.email, this.officeNumber);
+                                console.log(manager);
+                                
+                                team.push(manager);
+                                
+                            }).then(function() {
+                                addMore(); 
+                            })
+                    
+                        } else if (this.role === "Engineer") {
+                            inquirer.prompt([
+                                {
+                                    type: "input",
+                                    message: "What's the engineer\'s github username?",
+                                    name: "github", 
+                                }
+                    
+                            ]).then(function(r3) {
+                                this.github = r3.github;
+                    
+                                console.log(this.name);
+                                console.log(this.id);
+                                console.log(this.email);
+                                console.log(this.role);
+                                console.log(this.github);
+                    
+                                const engineer = new Engineer(this.name, this.id, this.email, this.github);
+                                console.log(engineer)
+                                team.push(engineer);
+                                
+                                
+                                }).then(function() {
+                                    addMore(); 
+                                })
+                    
+                            } else {
+                                inquirer.prompt([
+                                    {
+                                    type: 'input',
+                                    message: "What's the Intern\'s school name?",
+                                    name: 'school'
+                                    }
 
+                                ]).then(function(r2){
+                                    this.school = r2.school;
+                                    console.log(this.school);
+                    
+                                    const school = new Intern(this.name, this.id, this.email, this.school);                
+                                    console.log(school);
+                                    team.push(school);
+                                    
+                    
+                                }).then(function() {
+                                    addMore(); 
+                                })
+                            }    
+
+                    } else {
+                        console.log("Please, enter a valid email address.")
+                        email();
+                    }
+                }) 
             }
-    
-        }  else {
+        })
+    }
 
-            // start();
-            console.log ("Please, enter a valid email address.");
-        } 
-    })
-}
 
 function addMore() {
     inquirer.prompt([
