63a76ff46506aa263e5490a2672ca282b2ea46c5
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-17 22:04:29 -0400

    created the inquire questions

--- app.js
+++
@@ -1,8 +0,0 @@
-const Main = require("./templates/main.js");
-// const Manager = require("./templates/manager.js");
-// const Engineer = require("./templates/engineer.js");
-// const Intern = require("./templates/intern.js");
-
-const main = new Main();
-
-main.employeeName();
